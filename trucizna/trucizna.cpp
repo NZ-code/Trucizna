d76f9d7e6e4c897e6b5dba25ce0375a2dd8568aa
nikita zenevich <zenevichnikita2001@gmail.com>
2020-11-19 22:13:32 +0100

    First fully-working version

--- trucizna/trucizna.cpp
+++ trucizna/trucizna.cpp
@@ -1,5 +1,8 @@
 ﻿#include <iostream>
 #define MAX_CARDS 174
+
+#define MAX_O 20
+#define MIN_O 1
 using namespace std;
 
 enum  colors_t {
@@ -63,15 +66,7 @@ void show_deck(cards_t deck) {
         show_card(deck.deck[i]);
     }
 }
-void add_gcards(cards_t *main_deck, unsigned int g, unsigned int gv) {
-    // create and add green cards to a main deck
-    for (int i = 0; i < int(g); i++) // amount of green cards
-    {
-        card_t green_card = { green, gv }; //gv - green card value
-        main_deck->deck[i] = green_card;
-        main_deck->length++;
-    }
-}
+
 void add_cards(cards_t* main_deck, unsigned int len, unsigned int value, colors_t color) {
     // create and add green cards to a main deck
     for (int i =0; i < int(len); i++) // amount of green cards
@@ -84,46 +79,43 @@ void add_cards(cards_t* main_deck, unsigned int len, unsigned int value, colors_
 
 int main()
 {
-   /* "Trucizna game!\n";
-     (1 <= k <= 6, 
-     1 <= g <= 9,
-     1<= gv <= 10,
-     1 <= o <= 20;
-    */
+   
 
     
     unsigned int k, g, gv, o; // nums of...cauldrons |green card| value of green card| other color card(not green)
     do {
         cin >> k >> g >> gv >> o;
-        if (1 <= k && k <= 6 && 1 <= g && g <= 9 && 1 <= gv && gv <= 10 && 1 <= o && o <= 20) break;
+        if (1 <= k && k <= 6 && 1 <= g && g <= 9 && 1 <= gv && gv <= 10 && 1 <= o && o <= MAX_O) break;
     } while (true);
 
-    
-
-    //creating main deck
-    unsigned int amount_of_cars =  g + k*o;
+   
     cards_t main_deck;
     // adding green cards
     add_cards(&main_deck, g, gv,green);
 
+
+    int arr_values[MAX_O];
     //adding other cards in main_deck
-    for (int j = 0; j < int(k); j++) //iterate by colors(int)
-    
+    for (int i = 0; i < int(o); i++) // nums of no-green cards
     {   
         unsigned int card_value;
         cin >> card_value;
-        for (int i = 0; i < int(o); i++) // nums of no-green cards
-        {
-            
-            add_cards(&main_deck, 1, card_value, colors_t(j+1));
-            
-        }
+        arr_values[i] = card_value;
+       
     }
     
-    
+    for (int j = 0; j < int(k); j++) //iterate by colors(int)
+    {
+        
+       
+        for (int i = 0; i < int(o); i++) // nums of no-green cards
+        {
+            unsigned int card_value = arr_values[i];
+            add_cards(&main_deck, 1, card_value, colors_t(j + 1));
 
+        }
+    }
     show_deck(main_deck);
-    
     
 }
 
