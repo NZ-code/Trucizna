83a348383c244313a76f3db5c920695f6276d08a
NZ-code <59285334+NZ-code@users.noreply.github.com>
2020-11-19 22:53:11 +0100

    Update trucizna.cpp
    fill_deck is a new function now

--- trucizna/trucizna.cpp
+++ trucizna/trucizna.cpp
@@ -1,7 +1,14 @@
-﻿#include <iostream>
+#include <iostream>
 #define MAX_CARDS 174
 
-#define MAX_O 20
+#define MAX_OTHER 20
+#define MIN_OTHER 1
+#define MAX_G_VAL 20
+#define MIN_G_VAL 1
+#define MAX_GREEN 9
+#define MIN_GREEN 1
+#define MAX_CAULDRONS 6
+#define MIN_CAULDRONS 1
 #define MIN_O 1
 using namespace std;
 
@@ -18,10 +25,10 @@ typedef unsigned int val;
 
 struct card_t {
     colors_t card_color;
-    val card_value;  
+    val card_value;
 };
 struct  cards_t
-{   
+{
     int length = 0;
     card_t deck[MAX_CARDS]{};
 };
@@ -31,7 +38,7 @@ struct  cauldron
     cards_t cauldron_deck;
 };
 void show_card(card_t card) {
-    cout << card.card_value<<" ";
+    cout << card.card_value << " ";
     switch (card.card_color) {
     case green:
         cout << "green";
@@ -69,53 +76,60 @@ void show_deck(cards_t deck) {
 
 void add_cards(cards_t* main_deck, unsigned int len, unsigned int value, colors_t color) {
     // create and add green cards to a main deck
-    for (int i =0; i < int(len); i++) // amount of green cards
+    for (int i = 0; i < int(len); i++) // amount of green cards
     {
-        card_t card = { color, value }; 
+        card_t card = { color, value };
         main_deck->deck[main_deck->length] = card;
         main_deck->length++;
     }
 }
-
-int main()
-{
-   
-
-    
-    unsigned int k, g, gv, o; // nums of...cauldrons |green card| value of green card| other color card(not green)
-    do {
-        cin >> k >> g >> gv >> o;
-        if (1 <= k && k <= 6 && 1 <= g && g <= 9 && 1 <= gv && gv <= 10 && 1 <= o && o <= MAX_O) break;
-    } while (true);
-
-   
-    cards_t main_deck;
+void fill_deck(cards_t* main_deck, int k, int g, int gv, int o) {
     // adding green cards
-    add_cards(&main_deck, g, gv,green);
+    add_cards(main_deck, g, gv, green);
 
 
-    int arr_values[MAX_O];
+    int arr_values[MAX_OTHER];
     //adding other cards in main_deck
     for (int i = 0; i < int(o); i++) // nums of no-green cards
-    {   
+    {
         unsigned int card_value;
         cin >> card_value;
         arr_values[i] = card_value;
-       
+
     }
-    
+
     for (int j = 0; j < int(k); j++) //iterate by colors(int)
     {
-        
-       
+
+
         for (int i = 0; i < int(o); i++) // nums of no-green cards
         {
             unsigned int card_value = arr_values[i];
-            add_cards(&main_deck, 1, card_value, colors_t(j + 1));
+            add_cards(main_deck, 1, card_value, colors_t(j + 1));
 
         }
     }
-    show_deck(main_deck);
-    
 }
+int main()
+{
 
+
+
+    unsigned int k, g, gv, o; // nums of...cauldrons |green card| value of green card| other color card(not green)
+    do {
+        cin >> k >> g >> gv >> o;
+        if ((MIN_CAULDRONS <= k && k <= MAX_CAULDRONS)\
+            && (MIN_GREEN <= g && g <= MAX_GREEN)\
+            && (MIN_G_VAL <= gv && gv <= MAX_G_VAL)\
+            && (MIN_OTHER <= o && o <= MAX_OTHER)) break;
+        else {
+            cout << "Looks like you entered wrong values";
+        }
+    } while (true);
+
+
+    cards_t main_deck;
+    fill_deck(&main_deck, k, g, gv, o);
+    show_deck(main_deck);
+
+}
