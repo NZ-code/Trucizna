e74c3482713836a21e0dbe173590a8bebb9b0f3e
nikita zenevich <zenevichnikita2001@gmail.com>
2020-11-19 18:00:05 +0100

    Adding rules

--- trucizna/trucizna.cpp
+++ trucizna/trucizna.cpp
@@ -1,10 +1,13 @@
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 int main()
 {
     cout << "Trucizna game!\n";
-    int a = 3;
-    cout << a;
+    cout << "(1 <= k <= 6, 1 <= g <= 9, 1<= gv <= 10, 1 <= o <= 20)\n";
+    cout << "Enter k - liczbe kociolkow odpowiadajaca liczbie kolorow kart z wylaczeniem zielonego\n";
+    cout << "g - liczbe zielonych kart w talii,\n";
+    cout << "gv - wartosc zielonych kart\n";
+    cout << "o - liczba kart dla kazdego innego koloru niz zielony\n";
 }
 
